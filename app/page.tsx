40266a47992be2b2927215871b5b23d58c2e8aef
Matthew Stanciu <mattbstanciu@gmail.com>
2024-09-20 13:23:26 -0400

    Display object on page when signed in

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import { SignInButton } from "@/components/sign-in-button";
 import { SignOutButton } from "@/components/sign-out-button";
 
 export default async function Home() {
-  let session = await auth();
+  const session = await auth();
+  const profile = session?.user;
   console.log({ session });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {session ? (
         <div className="flex flex-col justify-center">
-          <p>signed in!</p>
+          <h1 className="text-2xl font-bold">You&apos;re authenticated!</h1>
+          <pre>{JSON.stringify(profile, null, 2)}</pre>
           <SignOutButton />
         </div>
       ) : (
